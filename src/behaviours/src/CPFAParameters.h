b2eb7ec3dc8a4bddcb317981e12f11c5c68bef6a
lukey11 <lqcobra@hotmail.com>
2018-05-29 17:00:12 -0600

    Test the CPFA parameters for obstacle cases

--- src/behaviours/src/CPFAParameters.h
+++ src/behaviours/src/CPFAParameters.h
@@ -3,7 +3,19 @@
 
 struct CPFAParameters {
   const float random_seed = -1;
-  //For clustered
+  //***For no obst.
+  /*const float probability_of_switching_to_searching = 0.2;
+  const float probability_of_returning_to_nest = 0.05;
+  // In radians
+  const float uninformed_search_variation = 0.7; //range [0, pi]
+  // Lower causes it to decay slower
+  const float rate_of_informed_search_decay = 0.1;
+  // Reduce these to increase probability
+  const float rate_of_following_site_fidelity = 6; //0.3; original is 0.3; range [0, 20]; 0 -> 1; 20 -> 0
+  const float rate_of_laying_pheromone = 8; //range [0, 20]; 0 -> 1; 20 -> 0
+  const float rate_of_pheromone_decay = 0.1; //range [0, 1];
+  */
+  //***For obst.
   const float probability_of_switching_to_searching = 0.2;
   const float probability_of_returning_to_nest = 0.05;
   // In radians
@@ -14,8 +26,6 @@ struct CPFAParameters {
   const float rate_of_following_site_fidelity = 6; //0.3; original is 0.3; range [0, 20]; 0 -> 1; 20 -> 0
   const float rate_of_laying_pheromone = 8; //range [0, 20]; 0 -> 1; 20 -> 0
   const float rate_of_pheromone_decay = 0.1; //range [0, 1];
-  
-  
 };
 
 #endif
