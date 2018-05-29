fa55016686b35afa46503c3e74674380320f2c16
lukey11 <lqcobra@hotmail.com>
2018-05-29 17:49:47 -0600

    Tuning the CPFA parameters for obstacle cases

--- src/behaviours/src/CPFAParameters.h
+++ src/behaviours/src/CPFAParameters.h
@@ -17,14 +17,14 @@ struct CPFAParameters {
   */
   //***For obst.
   const float probability_of_switching_to_searching = 0.2;
-  const float probability_of_returning_to_nest = 0.05;
+  const float probability_of_returning_to_nest = 0.005;
   // In radians
-  const float uninformed_search_variation = 0.7; //range [0, pi]
+  const float uninformed_search_variation = 0.4; //range [0, pi]
   // Lower causes it to decay slower
   const float rate_of_informed_search_decay = 0.1;
   // Reduce these to increase probability
-  const float rate_of_following_site_fidelity = 6; //0.3; original is 0.3; range [0, 20]; 0 -> 1; 20 -> 0
-  const float rate_of_laying_pheromone = 8; //range [0, 20]; 0 -> 1; 20 -> 0
+  const float rate_of_following_site_fidelity = 10; //0.3; original is 0.3; range [0, 20]; 0 -> 1; 20 -> 0
+  const float rate_of_laying_pheromone = 14; //range [0, 20]; 0 -> 1; 20 -> 0
   const float rate_of_pheromone_decay = 0.1; //range [0, 1];
 };
 
