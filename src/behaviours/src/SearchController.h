5075babd07ea039bdd73cde917b89d5d8973544b
lukey15 <lqcobra@hotmail.com>
2018-06-11 12:28:36 -0600

    1.Update the GUI for 14X14 arena; 2. Tuning parameters for obstacle avoidance and pick up.

--- src/behaviours/src/SearchController.h
+++ src/behaviours/src/SearchController.h
@@ -7,6 +7,9 @@
 #include "CPFAParameters.h"
 #include "Pheromone.h"
 #include "Controller.h"
+#ifndef M_PI
+#define M_PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286
+#endif
 
 /**
  * This class implements the search control algorithm for the rovers. The code
@@ -91,7 +94,7 @@ private:
    */
   //void setPheromone();
   CPFAState cpfa_state = start_state;
-  int arena_size = 15.0;
+  int arena_size = 14.0;
   int local_resource_density; // An estimate of the density of the resources in the local region.
   //std::vector<Pheromone> pheromones; // Stores all pheromone trails
   //map<int, vector<Pheromone>> pheromones;// qilu 08/2017
